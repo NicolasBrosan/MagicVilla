d9ec9e5caabd7598c1fae719622d5834008a5f19
Nicolas Brosan <nicolasbrosan@hotmail.com>
2023-05-30 10:40:41 -0300

    DbContext - Inyeccion de dependencia

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,217 @@
+﻿using MagicVilla_API.Datos;
+using MagicVilla_API.Models;
+using MagicVilla_API.Models.Dto;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        private readonly ILogger<VillaController> _logger;
+        private readonly ApplicationDbContext _dbContext;
+
+        public VillaController(ILogger<VillaController> logger, ApplicationDbContext dbContext)//---> Se realiza la inyección de dependencia
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDto>> GetVillas()
+        {
+            _logger.LogInformation("Obtener las Villas");
+            return Ok(_dbContext.Villas.ToList());
+
+        }
+
+        [HttpGet("id", Name = "GetVilla")]//----> "GetVilla" es el nombre de la ruta
+        [ProducesResponseType(StatusCodes.Status200OK)]//----> Documenta el status
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDto> GetVilla(int id)
+        {
+            if(id == 0)
+            {
+                _logger.LogError("Error al traer Villa con Id", + id);
+                return BadRequest();//----> Se encuentra mal la solicitud. Cod 400.
+            }
+
+            //var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+            var villa = _dbContext.Villas.FirstOrDefault(v => v.Id == id);
+
+            if(villa == null)
+            {
+                return NotFound();//----> No encontrado. Cod 404.
+            }
+
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDto> CrearVilla([FromBody] VillaDto villaDto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if(_dbContext.Villas.FirstOrDefault(v => v.Nombre.ToLower() == villaDto.Nombre.ToLower()) != null)
+            {
+                ModelState.AddModelError("NombreExiste", "La Villa con ese nombre ya existe!");//---> Validación personalizada. 1er param nombre de la validación y el 2do param el msj q quiero mostrar.
+                return BadRequest(ModelState);
+            }
+
+            if(villaDto == null)
+            {
+                return BadRequest(villaDto);
+            }
+            if(villaDto.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            Villa modelo = new()
+            {
+                Nombre = villaDto.Nombre,
+                Detalle = villaDto.Detalle,
+                ImagenUrl = villaDto.ImagenUrl,
+                Ocupantes = villaDto.Ocupantes,
+                Tarifa = villaDto.Tarifa,
+                MetrosCuadrados = villaDto.MetrosCuadrados,
+                Amenidad = villaDto.Amenidad
+            };
+
+            _dbContext.Villas.Add(modelo);
+            _dbContext.SaveChanges();
+
+            return CreatedAtRoute("GetVilla", new {id = villaDto.Id}, villaDto);//----> Se dirige a la ruta que le indicamos
+        }
+
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteVilla (int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = _dbContext.Villas.FirstOrDefault(v => v.Id == id);
+            if(villa == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Villas.Remove(villa);
+            _dbContext.SaveChanges();
+
+            return NoContent();
+
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult UpdateVilla(int id, [FromBody] VillaDto villaDto)
+        {
+            if(villaDto == null || id != villaDto.Id)
+            {
+                return BadRequest();
+            }
+
+            //var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+            //villa.Nombre = villaDto.Nombre;
+            //villa.Ocupantes = villaDto.Ocupantes;
+            //villa.MetrosCuadrados = villaDto.MetrosCuadrados;
+
+            Villa modelo = new()
+            {
+                Id = villaDto.Id,
+                Nombre = villaDto.Nombre,
+                Detalle = villaDto.Detalle,
+                ImagenUrl = villaDto.ImagenUrl,
+                Ocupantes = villaDto.Ocupantes,
+                Tarifa = villaDto.Tarifa,
+                MetrosCuadrados = villaDto.MetrosCuadrados,
+                Amenidad = villaDto.Amenidad
+            };
+
+            _dbContext.Villas.Update(modelo);
+            _dbContext.SaveChanges();
+
+            return NoContent();            
+        }
+
+
+        [HttpPatch("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDto> patchDto)
+        {
+            if (patchDto == null || id == 0)
+            {
+                return BadRequest();
+            }
+
+            //var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+
+            var villa = _dbContext.Villas.AsNoTracking().FirstOrDefault(v => v.Id == id);//---> AsNoTracking() => No rastrea los cambios. Esto significa que cualquier cambio realizado en los objetos no se reflejará en la base de datos al guardar los cambios.
+
+            VillaDto villaDto = new()//---> antes de actualizar el registro, colocamos los cambios temporalmente en un modelo de VillaDto.
+            {
+                Id = villa.Id,
+                Nombre = villa.Nombre,
+                Detalle = villa.Detalle,
+                ImagenUrl = villa.ImagenUrl,
+                Ocupantes = villa.Ocupantes,
+                Tarifa = villa.Tarifa,
+                MetrosCuadrados = villa.MetrosCuadrados,
+                Amenidad = villa.Amenidad
+            };
+
+            if(villa == null)
+            {
+                return BadRequest();
+            }
+
+            patchDto.ApplyTo(villaDto, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Villa modelo = new()
+            {
+                Id = villaDto.Id,
+                Nombre = villaDto.Nombre,
+                Detalle = villaDto.Detalle,
+                ImagenUrl = villaDto.ImagenUrl,
+                Ocupantes = villaDto.Ocupantes,
+                Tarifa = villaDto.Tarifa,
+                MetrosCuadrados = villaDto.MetrosCuadrados,
+                Amenidad = villaDto.Amenidad
+            };
+
+            _dbContext.Villas.Update(modelo);
+            _dbContext.SaveChanges();
+            return NoContent();
+        }
+    }
+}
